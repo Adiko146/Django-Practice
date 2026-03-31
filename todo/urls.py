34af80a3015704076e054238c4079c5d40716b37
Adiko146 <abikulabdimanap@gmail.com>
2026-03-31 11:04:07 +0500

    fix root url

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("main/", views.index, name='index'),
+    path("", views.index, name='index'),
     path('create/', views.task_create, name='task_create'),
     path('update/<int:pk>/', views.task_update, name='task_update'),
     path('delete/<int:pk>/', views.task_delete, name='task_delete'),
